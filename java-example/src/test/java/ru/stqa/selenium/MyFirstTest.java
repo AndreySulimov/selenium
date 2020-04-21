5c48719f749b2176c15f61af546e23c250e17081
AndreySulimov <60839802+AndreySulimov@users.noreply.github.com>
2020-04-21 23:34:11 +0300

    В MyFirstTest добавлен ввод с экранной клавиатуры

--- java-example/src/test/java/ru/stqa/selenium/MyFirstTest.java
+++ java-example/src/test/java/ru/stqa/selenium/MyFirstTest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class MyFirstTest {
@@ -18,12 +20,15 @@ public class MyFirstTest {
   @Before
   public void start() {
     driver = new ChromeDriver();
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     wait = new WebDriverWait(driver, 10);
   }
 
   @Test
   public void myFirstTest() {
     driver.get("https://www.google.ru/");
+    driver.findElement(By.cssSelector("span.hOoLGe")).click();
+    driver.findElement(By.id("K32")).click();
     driver.findElement(By.name("q")).sendKeys("Hello, world!");
     driver.findElement(By.name("btnK")).click();
     wait.until(titleIs("Hello, world! - Поиск в Google"));
